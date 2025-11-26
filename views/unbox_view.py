f5ce2005b57ff846e0048a9b8237b0d9f9bf204b
gabrielbx7 <gabriel033.lemos@gmail.com>
2025-11-26 19:25:46 -0300

    [VIEW] Depuraçao da Tela Principal

---
+++ views/unbox_view.py
@@ -0,0 +1,15 @@
+"""
+import flet as ft
+import sqlite3
+class CategoriaView:
+    def __init__(self,db):
+        self.db = db
+
+    def criar_categoria(self,nome):
+        conn = sqlite3.conect(self,db)
+        cursor = conn.cursor()
+        try
+
+
+        
+"""
